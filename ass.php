fbc7920dbb0d1cb283bbf098860cb024af05549e
baphont <baphont@gmail.com>
2014-06-13 16:31:05 +0800

    我不要搞基YAY

--- ass.php
+++ ass.php
@@ -1,8 +1,33 @@
 <!DOCTYPE HTML PUBLIC “-//W3C//DTD HTML 4.01 Transitional//EN" “http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<title>Fuck i'm fool|ASS</title>
+<title>Fuck i'm fool | ASS</title>
 
 <link href="style.css" rel="stylesheet" type="text/css" />
     
 </head>
+
+<body>
+    <div id="hole">
+    <h1>Tony loves TORI.</h1>
+        <div id="fuck">
+        <h2>He sucks my Dick YAY</h2>
+        </div>
+        <div id="gayguy">
+        <h3>幹,你這基老</h3>
+        </div>
+ 
+    
+    <div id="ohh">
+    <a href="index.php"><h5>尻回上一頁</h5></a>
+    </div>
+    
+    
+   </div>
+    
+    
+    
+    
+</body>
+    
+</html>
