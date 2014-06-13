7738295b0d536e48c074387d9eeda1c1d424f7ed
baphont <baphont@gmail.com>
2014-06-13 12:30:24 +0800

    funny...

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML PUBLIC “-//W3C//DTD HTML 4.01 Transitional//EN" “http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Fuck i'm fool</title>
+
+<link href="style.css" rel="stylesheet" type="text/css" />
+    
+</head>
+
+<body>
+<div>
+    <div id="head";>
+    <h1><a href="index.php"><?php echo "suck my ass"; ?></a></h1>
+        <div id="fuckin">
+        <h2>kick ur ass</h2>
+        </div>
+    </div>
+    <div id="cat";>
+        <ul id="haha">
+            <li><a href="ass.php">ASS</a></li>
+            <li><a href="">Dick</a></li>
+        </ul>
+    </div>
+</div>
+    
+    
+</body>
+
+</html>
